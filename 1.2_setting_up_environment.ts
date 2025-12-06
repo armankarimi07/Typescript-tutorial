1dd0a66ef46ebc1a76c41e159c829aeed40a731e
armk7 <armankarimy7@gmail.com>
2025-12-06 11:56:34 +0330

    1.2 setting up environment

---
+++ 1.2_setting_up_environment.ts
@@ -0,0 +1,15 @@
+// Node.js and a typescript compiler is required to work with typescript
+// typescript compiler is a node module that compiles ts into javascript
+
+// download and install node.js if not already
+// verify node installtion with: node -v
+
+// then run: npm install -g typescript
+// (-g will install the module globally)
+// test the compiler version: tsc --v
+
+// to run TypeScript code directly on Node.js without precompiling, you can use the tsx module.
+// > npm install -g tsx
+
+
+// vscode and Live Server is recommended
